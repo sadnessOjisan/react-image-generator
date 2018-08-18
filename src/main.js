e165d03b0bdae586e05ac5025313829914412181
sadness_ojisan <sadness.ojisan@gmail.com>
2018-08-18 14:24:55 +0900

    [update] can upload img

--- src/main.js
+++ src/main.js
@@ -1,7 +1,8 @@
 import React, { Component } from "react";
 import ReactDOM from "react-dom";
 import styled from "styled-components";
-import './reset.css'
+import Dropzone from "react-dropzone";
+import "./reset.css";
 
 class App extends Component {
   constructor(props) {
@@ -10,17 +11,56 @@ class App extends Component {
       inputImages: [],
       generatedImage: null
     };
+    this._handleAddImage = this._handleAddImage.bind(this);
+  }
+
+  _handleAddImage(e) {
+    console.log("<_handleAddImage> e: ", e);
+    const addedImage = e[0];
+    this.setState({
+      ...this.state,
+      inputImages: [...this.state.inputImages, addedImage.preview]
+    });
   }
 
   render() {
-  return (<Container><p> hello world </p></Container>);
+    const { inputImages } = this.state;
+    console.log("render inputImages: ", inputImages);
+    return (
+      <Container>
+        <ImageList>
+          {inputImages.map(image => {
+            return(<ImageWrapper>
+              <TrimedImg src={image} />
+            </ImageWrapper>);
+          })}
+        </ImageList>
+        <Dropzone onDrop={this._handleAddImage} />
+      </Container>
+    );
   }
 }
 
 const Container = styled.div`
-width: 100%; 
-display: flex;
-justify-content: center;
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  flex-flow: column nowrap;
+`;
+
+const ImageList = styled.div`
+  overflow-x: scroll;
+  width: 100%;
+`;
+
+const ImageWrapper = styled.div`
+width: 300px;
+height: 300px;
+`
+
+const TrimedImg = styled.img`
+max-width: 100%; 
+max-height: 100%;
 `
 
 ReactDOM.render(<App />, document.getElementById("root"));
